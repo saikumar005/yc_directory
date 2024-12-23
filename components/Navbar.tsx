c6f27e5c2da0f4071e9f57a9864cb6c4e7d5e2eb
sai <saikumarthokamuri@gmail.com>
2024-12-23 15:57:37 +0530

    initial commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-between items-center bg-white backdrop-blur'>
+        
+    </div>
+  )
+}
+
+export default Navbar
